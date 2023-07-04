9add6610ea1298ead09c08aa4e8361b49af43920
Alexandre.scht <alexandreschecht@gmail.com>
2023-07-04 16:45:34 +0200

    front index

--- frontend/src/pages/index.jsx
+++ frontend/src/pages/index.jsx
@@ -1,5 +1,14 @@
+import Login from "./login"
+import HomePage from "./home"
+
+import useAppContext from "../hook/AppContext"
+
 const Home = () => {
-  return <div>Home page</div>
+  const {
+    state: { session },
+  } = useAppContext()
+
+  return session ? <HomePage /> : <Login />
 }
 Home.isPublic = true
 export default Home
