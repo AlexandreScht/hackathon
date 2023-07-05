5536a240bf1dcbe34c83c2bd6be4237c2769f166
AntoineMosleh <toni.mosleh@outlook.fr>
2023-07-05 09:11:38 +0200

    minos changes

--- frontend/src/pages/home/index.jsx
+++ frontend/src/pages/home/index.jsx
@@ -1,5 +1,5 @@
-const Home = () => {
-  return <div>Home</div>
+const HomePage = () => {
+  return <h1>Hello c'est la page principal</h1>
 }
-Home.isPublic = true
-export default Home
+HomePage.isPublic = true
+export default HomePage
